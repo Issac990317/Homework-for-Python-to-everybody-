bb5feec12620822e18c693eeb14e6e375265a134
Issac990317 <110734286+Issac990317@users.noreply.github.com>
2024-10-24 09:16:54 +0800

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,36 @@
+"""
+#定义函数thing的使用方法
+def thing():
+    print("hello")
+    print("fun")
+
+thing()
+print("zip")
+thing()
+
+
+big = max("hello world")
+print(big)
+
+tiny = min("hello world")
+print(tiny)
+
+
+print(float(99) / 100)
+
+i = 42
+print(type(i))
+
+f = float(i)
+print(f)
+
+print(type(f))
+
+print(1 + 2 * float(3) / 4 - 5)
+"""
+
+sval = "123"
+ival = int(sval)
+print(type(ival))
+print(ival + 1)
+
