f19d68b930536851c28ed92c0f12254249b48d91
Issac990317 <110734286+Issac990317@users.noreply.github.com>
2024-10-30 10:18:06 +0800

    Add files via upload

--- Assignment 8.5.py
+++ Assignment 8.5.py
@@ -1,3 +1,4 @@
+"""
 fname = input("Enter file name: ")
 fh = open(fname)
 count = 0
@@ -7,4 +8,15 @@ for line in fh:
         list = line.split()
         print(list[1])
         count = count + 1
+print("There were", count, "lines in the file with From as the first word")
+"""
+name = input("Enter file name: ")
+fh = open(name)
+count = 0
+for line in fh:
+    line.rstrip()
+    if line.startswith("From:"):
+        list1 = line.split()
+        count = count + 1
+        print(list1[1])
 print("There were", count, "lines in the file with From as the first word")
